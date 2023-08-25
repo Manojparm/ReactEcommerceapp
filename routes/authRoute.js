2c0af6b99aeaec5385a93c2f7ab7faacadd788ad
Manojparm <123813997+Manojparm@users.noreply.github.com>
2023-08-25 19:03:48 +0530

    router authentication

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -16,5 +16,8 @@ router.post('/login',loginController)
 //test || Method get
 router.get('/test',requireSignIn,isAdmin,testController)
 
-
+//protectd route auth
+router.get('/user-auth',requireSignIn,(req,res)=>{
+    res.status(200).send({ok:true})
+})
 export default router
